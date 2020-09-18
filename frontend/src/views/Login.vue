f2f9e9b948b96c9678db5a1583327ead77325f89
viviennoel <52512132+viviennoel@users.noreply.github.com>
2020-09-18 20:14:37 +0200

    Modification du design et vérifications

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,174 @@
+<template>
+    <main class="main_wrapp" id="main_wrapp_login">
+        <div class="col-md-6 pt-5">
+            <form class="cadre w-50 m-auto">
+                <h1 id="title_signin">Connexion!</h1>
+                <div class="container ">
+                    <div class="form-group ">
+                        <label for="inputemail ">Mon e-mail</label>
+                        <input type="text" class="form-control" id="inputemail" v-model="dataLogin.email" />
+                    </div>
+                    <div class="form-group ">
+                        <label for="inputPassword">Mon mot de passe</label>
+                        <input type="password" class="form-control" id="inputPassword" v-model="dataLogin.password" />
+                    </div>
+                    <small> Pas de compte? <router-link class="redirection-signup" to="/signup">Inscription!</router-link></small> <br>
+                    <button @click.prevent="logIn" type="submit" class="btn btn-secondary mb-3 mt-3">Connexion 🔒</button>
+                </div>
+            </form>
+        </div>
+        <modale v-bind:revele="revele" v-bind:toggleModale="toggleModale"></modale>
+    </main>
+
+</template>
+
+<script>//import de la bibliothèque et d'axios pour les requêtes
+
+    import axios from "axios";
+    import { mapState } from "vuex";
+    // import du component Modale pour l'alerte du mot de passe ou email incorrect
+    import modale from "../components/modale"
+
+
+    export default {
+        name: "SignUp",
+        data() {
+            return {
+                dataLogin: {
+                    email: null,
+                    password: null
+                },
+                msg: "",
+                revele: false
+            };
+        },
+        components: {
+            'modale': modale
+        },
+        computed: {
+            ...mapState(["user"])
+        },
+        methods: {
+            // fonction modale qui revele ou non la modale si il y a une erreur
+            toggleModale: function () {
+                this.revele = !this.revele
+            },
+
+            // requête pour connecter l'user déja existant
+            logIn() {
+                if (
+                    this.dataLogin.email !== null || this.dataLogin.password !== null
+                ) {
+                    axios
+                        .post("http://localhost:3000/api/user/login", this.dataLogin)
+                        .then(response => {
+                            localStorage.setItem('token', response.data.token)
+                            this.$router.push({ path: 'Wall' })
+                        })
+                        .catch(error => {
+                            console.log(error)
+                            this.revele = !this.revele
+                        })
+
+                } else {
+                    console.log("oops !");
+                }
+            }
+        }
+    };</script>
+
+<style scope>
+    #title_signin {
+        color: #000000;
+        font-size: 2em;
+        position: relative !important;
+        top: 0 !important;
+    }
+
+    .cadre {
+        padding: 10px;
+        border: #000000 1px solid !important;
+        background-color: #ffffff5e;
+        animation: fade 1s ease-in forwards;
+    }
+
+        .cadre:hover {
+            background-color: #ffffffc7;
+        }
+
+    .btn {
+        background-color: #6c757d !important;
+    }
+
+    .main_wrapp {
+        padding-top: 20%;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: right;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    #main_wrapp_login {
+        background-image: url('../assets/bg_login.jpg');
+    }
+
+    #main_wrapp_login h1 {
+        font-size: 1.5em;
+        font-weight: 300 !important;
+        color: #a2a1a1;
+        padding: 10px 20px;
+        text-align: center;
+        width: 100%;
+        margin: auto;
+    }        
+
+    .redirection-signup {
+        color: #6c757d;
+        text-decoration: none;
+    }
+
+        .redirection-signup:hover {
+            color: #fd2d01;
+        }
+
+    .body-intro {
+        margin-top: 25%;
+    }
+
+    .main_wrapp h1 {
+        color: #000000;
+        font-size: 2em;
+        color: #ffffff;
+        position: relative;
+        top: 0;
+        padding: 0px 20px;
+        text-align: center;
+        width: 100%;
+        margin: auto;
+    }
+
+    @keyframes fade {
+        0% {
+            opacity: 0;
+            transform: translateY(-30px);
+        }
+
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @-webkit-keyframes fade {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+</style>
---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,152 @@
+<template>
+    <main class="main_wrapp row" id="main_wrapp_signup">
+        <div class="col-md-6">
+            <form class="cadre w-50 m-auto">
+
+                <h1 id="title_signup">Rejoignez nous!</h1>
+                <div class="container">
+                    <div class="form-group ">
+                        <label for="inputEmail"> Mon e-mail</label>
+                        <input type="email" class="form-control" id="inputEmail" v-model="dataSignup.email" />
+                    </div>
+                    <div class="form-group">
+                        <label for="inputUsername"> Mon nom d'utilisateur</label>
+                        <input type="text" class="form-control" id="inputUsername" v-model="dataSignup.username" />
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword"> Mon mot de passe</label>
+                        <input type="password" class="form-control" id="inputPassword" v-model="dataSignup.password" />
+                        <small>Votre mot de passe doit contenir au moins 6 caractères dont 1 majuscule, 1 minuscule et un chiffre</small>
+                    </div>
+                    <button @click.prevent="sendSignup" type="submit" class="btn btn-secondary mb-3 mt-3">Créer mon compte 🔒</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</template>
+
+<script>//import de la bibliothèque et d'axios pour les requêtes
+
+    import { mapState } from "vuex";
+    import axios from "axios";
+
+
+    export default {
+        name: "SignUp",
+        data() {
+            return {
+                dataSignup: {
+                    username: null,
+                    email: null,
+                    password: null
+                },
+                msg: ""
+            };
+        },
+        computed: {
+            ...mapState(["user"])
+        },
+        methods: {
+            // requête pour créer un user, sécurité grâce au regex
+            sendSignup() {
+                const regexPassword = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/;
+                const regexEmail = /^[a-z0-9._-]+@[a-z0-9.-]{2,}[.][a-z]{2,3}$/;
+                const usernameRegex = /^[a-zA-Z ,.'-]+$/;
+                if (
+                    (this.dataSignup.email !== null || this.dataSignup.username !== null || this.dataSignup.password !== null) &&
+                    (regexPassword.test(this.dataSignup.password) && regexEmail.test(this.dataSignup.email) && usernameRegex.test(this.dataSignup.username))
+                ) {
+                    axios
+                        .post("http://localhost:3000/api/user/signup", this.dataSignup)
+                        .then(response => {
+                            console.log(response);
+
+                            //Réinitialisation des champs après saisie
+                            this.dataSignup.email = null;
+                            this.dataSignup.username = null;
+                            this.dataSignup.password = null;
+                            this.$router.push({ path: '/login' });
+                        })
+                        .catch(error => console.log(error));
+                } else {
+                    alert("Désolé, vous n'avez pas rempli les champs correctement ! ");
+                }
+            }
+        }
+    };</script>
+
+<style scope>
+
+    .cadre {
+        padding: 10px;
+        border: #000000 1px solid !important;
+        background-color: #ffffff5e;
+        animation: fade 1s ease-in forwards;
+    }
+
+        .cadre:hover {
+            background-color: #ffffffc7;
+        }
+
+    .btn {
+        background-color: #6c757d !important;
+    }
+
+    .main_wrapp {
+        padding-top: 20%;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: right;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    #main_wrapp_signup {
+        background-image: url('../assets/bg_login.jpg');
+    }
+
+        #main_wrapp_signup h1 {
+            font-size: 1.5em;
+            font-weight: 300 !important;
+            color: #a2a1a1;
+            padding: 10px 20px;
+            text-align: center;
+            width: 100%;
+            margin: auto;
+        }
+
+    .body-intro {
+        margin-top: 25%;
+    }
+
+    #title_signup {
+        color: #000000;
+        font-size: 2em;
+        position: relative !important;
+        top: 0 !important;
+    }
+
+    @keyframes fade {
+        0% {
+            opacity: 0;
+            transform: translateY(-30px);
+        }
+
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @-webkit-keyframes fade {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+</style>
